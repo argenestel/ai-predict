4bc8ce71ff82dde7aef3ec24ce38af0f366b04e7
argenestel <49126445+argenestel@users.noreply.github.com>
2024-09-21 04:22:01 +0530

    new horizon dashboard and fixes in predictions

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,35 +1,52 @@
-import React, { ReactNode } from 'react';
-import AppWrappers from './AppWrappers';
-// import '@asseinfo/react-kanban/dist/styles.css';
-// import '/public/styles/Plugins.css';
-import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core';
-import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { WagmiProvider } from 'wagmi';
-import { config } from 'config';
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import {
+  createConfig,
+  WagmiProvider,
+} from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { http } from 'viem';
+import {  sepolia } from 'viem/chains';
 
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import AppWrappers from "./AppWrappers";
+import { ReactNode } from "react";
 
+const config = createConfig({
+  chains: [sepolia ],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [sepolia.id]: http(),
+  },
+});
+  
 const queryClient = new QueryClient();
+  
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body id={'root'}>
-        <AppWrappers>
-
-          <WagmiProvider config={config}>
-            <QueryClientProvider client={queryClient}>
-              <DynamicContextProvider
-                settings={{
-                  environmentId: '8d1a0fcf-94bc-4ca7-bbe0-0d36017f8084',
-                  walletConnectors: [EthereumWalletConnectors],
-                }}>
-                {children}
-              </DynamicContextProvider>
-
-            </QueryClientProvider>
-          </WagmiProvider>
-        </AppWrappers>
+    <body id={'root'}>
+      <AppWrappers>
+    <DynamicContextProvider
+      settings={{
+        // Find your environment id at https://app.dynamic.xyz/dashboard/developer
+        environmentId: "8d1a0fcf-94bc-4ca7-bbe0-0d36017f8084",
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>
+          {children}
+          </DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider> 
+    </DynamicContextProvider>
+    </AppWrappers>
       </body>
     </html>
   );
-}
+};
